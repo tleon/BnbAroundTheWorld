1db359dd1605cf5a4b41033d58b5193ff9e99621
Quentin LATOUR <Quentour69100@gmail.com>
2019-04-17 09:42:33 +0200

    debut insertion reservation bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,5 +22,58 @@ class HomeController extends AbstractController
     public function index()
     {
         return $this->twig->render('Home/index.html.twig');
+
+
+
+
+
+
+/*verification des données de reservation renvoyer par le calendrier
+*
+*
+*/
+
+
+//definition de tableau valide auquel on compare les valeurs renvoyer
+
+$validValues = array 
+(
+'country' => ["1","2","3","4","5"],
+'date' => ["date"],
+'numberGuest' => ["1","2","3","4","5","6","7","8","9","10"]
+);
+
+$error = array();
+
+
+
+
+function test_input($value) 
+{
+    $data = trim($value);
+    $data = stripslashes($value);
+    $data = htmlspecialchars($value);
+    return $value;
+}
+
+
+if ($_SERVER("REQUEST_METHOD") == "POST")
+{
+    foreach ($_POST as $key => $value)
+    {
+        if (empty($_POST[$value]) || !in_array($_POST[$value], $validValues[$value]))
+        {
+            $error[$key] = "erreur pendant la reservation, recharger la page et retentez";
+        }
+        else
+        {
+            $reservation[$key] = test_input($value);
+        }
+    }
+    if (empty($error))
+    {
+        $reservation;
+    }
+}
     }
 }
