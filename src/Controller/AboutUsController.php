be2e9157c86939280fde331582834ce47beba897
tleon <leone.thomas.92@gmail.com>
2019-04-22 23:17:58 +0200

    about us page, map, access ways, detail desc

---
+++ src/Controller/AboutUsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+class AboutUsController extends AbstractController
+{
+
+    /**
+     * Display admin home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function show()
+    {
+        return $this->twig->render('AboutUs/aboutUs.html.twig');
+    }
+
+    /**
+     * Allow connection to admin page
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    
+}
